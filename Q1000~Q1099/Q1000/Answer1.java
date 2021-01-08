447ba447b72c1e18a979a58735306f7d25942ff1
plitche <kysu728@naver.com>
2021-01-08 23:34:33 +0900

    Add Solution code
    Add Solution code of Q1000 at CodeUp with 2 ways

---
+++ Q1000~Q1099/Q1000/Answer1.java
@@ -0,0 +1,17 @@
+package Q1000;
+
+// @author Yongsoo Kwon
+// @URL https://www.codeup.kr/problem.php?id=
+// @How System.out.prinln()
+// @Memory 10932, @Time 84
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+		
+		//printStream의 println() 메소드를 이용해서 Hello 출력
+		System.out.println("Hello");
+		
+	}
+	
+}
