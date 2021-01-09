dcc64411d0ba4f733492875c1df3b98f17e6a976
plitche <kysu728@naver.com>
2021-01-09 13:30:44 +0900

    Add solution of Q1004
    Add solution of CodeUp's Question No.1004 with 2 ways.

---
+++ Q1001~Q1100/Q1004/Answer1.java
@@ -0,0 +1,20 @@
+package Q1004;
+
+// @author Yongsoo Kwon
+// @URL https://www.codeup.kr/problem.php?id=1004
+// @How System.out.prinln() + print()
+// @Memory 10984, @Time 71
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+		
+		//printStream의 println() 메소드를 이용해서 Hello 출력
+		System.out.print("\'Hello\'");	// print() 메소드는 줄바꿈을 하지 않는다.
+		// 역슬래쉬 +' : 특수 문자를 사용하기 위해서는 역슬래쉬를 문자 앞에 써주어야 한다.
+		
+		// 'Hello'만 출력되면 되서 print(), println() 어느것을 사용해도 무관하다.
+		// = System.out.println("\'Hello'\"); 
+
+	}
+}
---
+++ Q1001~Q1100/Q1004/Answer2.java
@@ -0,0 +1,38 @@
+package Q1004;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+// @author Yongsoo Kwon
+// @URL https://www.codeup.kr/problem.php?id=1004
+// @How BufferedWriter
+// @Memory 11080, @Time 93
+
+public class Answer2 {
+
+	public static void main(String[] args) throws IOException {
+		/*	
+		 * buffer(버퍼)
+		 * - 일시적으로 전송할 데이터를 보관하는 임시 메모리 영역
+		 * - 입출력 속도 향상에 도움이 된다. (많은 양을 처리할 때 성능에서 뛰어나다.)
+		 */
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		//버퍼를 이용한 출력
+		
+		//append() 메소드 대신 write() 메소드도 가능하다.
+		bw.append("\'").append("Hello").append("\'");
+		// == bw.append("\'Hello\'");
+		// write() 메소드는 연결해서 사용이 불가하여 나누어서 작성한다. 
+		/*
+		 * bw.write("\'");	
+		 * bw.write("Hello");
+		 * bw.write("\'");
+		 * ---------------------
+		 * bw.write("\'Hello\'");
+		 */
+		 
+		bw.flush();	//버퍼에 남아있는 데이터를 출력시킴(버퍼를 비우는 동작)
+		bw.close();
+	}
+}
