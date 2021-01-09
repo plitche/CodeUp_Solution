b334e6bb0041cd7141a546080765ede952cf75eb
plitche <kysu728@naver.com>
2021-01-09 12:52:40 +0900

    Add solution of Q1003
    Add solution of CodeUp's Question No.1003 with 2 ways.

---
+++ Q1001~Q1100/Q1003/Answer1.java
@@ -0,0 +1,23 @@
+package Q1003;
+
+// @author Yongsoo Kwon
+// @URL https://www.codeup.kr/problem.php?id=1003
+// @How System.out.prinln() + print()
+// @Memory 10964, @Time 81
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+	
+		//printStream의 println() 메소드를 이용해서 Hello 출력
+		System.out.print("Hello");	// print() 메소드는 줄바꿈을 하지 않는다.
+		System.out.print("\n");	// 역슬래쉬 + n : 공백 (그냥 띄어쓰기도 가능함)
+		System.out.println("World");
+		
+		// println()으로 출력
+		// System.out.println("Hello");
+		// System.out.println("World"); 
+		
+	}
+	
+}
