46f0dc1f2691a4ecfa0cc6256254fcb9afab0e45
plitche <kysu728@naver.com>
2021-01-09 12:54:20 +0900

    Modify wrong code
    Modify wrong print code written to try make space.
    change '\n' to '\s'

--- Q1001~Q1100/Q1002/Answer1.java
+++ Q1001~Q1100/Q1002/Answer1.java
@@ -11,7 +11,7 @@ public class Answer1 {
 		
 		//printStream의 println() 메소드를 이용해서 Hello 출력
 		System.out.print("Hello");	// print() 메소드는 줄바꿈을 하지 않는다.
-		System.out.print("\s");	// 역슬래쉬 + n : 공백 (그냥 띄어쓰기도 가능함)
+		System.out.print("\s");	// 역슬래쉬 +s : 공백 (그냥 띄어쓰기도 가능함)
 		System.out.println("World");
 		
 		// 한줄에 작성도 가능하다.
