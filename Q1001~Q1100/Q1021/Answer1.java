9aaabfaa03cb44a05c2d3ca9ba288f8376e87cb7
plitche <kysu728@naver.com>
2021-01-12 20:37:53 +0900

    Add solution of Q1021
    Add solution of CodeUp's Question No.1021
    This question is really simple.
    just print what you have input

---
+++ Q1001~Q1100/Q1021/Answer1.java
@@ -0,0 +1,19 @@
+package Q1021;
+
+import java.util.Scanner;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1021
+//@How Scanner
+//@Memory 12384, @Time 114
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+		// Scanner에 대한 설명은 Q1009 - Answer1 참고
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next());
+
+		sc.close();
+	}
+}
---
+++ Q1001~Q1100/Q1021/Answer2.java
@@ -0,0 +1,20 @@
+package Q1021;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1021
+//@How BufferedReader
+//@Memory 11092, @Time 85
+
+public class Answer2 {
+
+	public static void main(String[] args) throws IOException {
+		// BufferedReader, StringBuilder에 대한 설명은 Q1009 - Answer2 참고
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());
+		br.close();
+	}
+}
