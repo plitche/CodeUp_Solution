5e5053419173f2a49885ddb77d26c26fa2ff9e71
plitche <kysu728@naver.com>
2021-01-12 20:38:47 +0900

    Add solution of Q1022
    Add solution of CodeUp's Question No.1022
    This question is almost same with previous one.
    just print what you have inputed

---
+++ Q1001~Q1100/Q1022/Answer1.java
@@ -0,0 +1,18 @@
+package Q1022;
+
+import java.util.Scanner;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1022
+//@How Scanner
+//@Memory 13024, @Time 114
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+		// Scanner에 대한 설명은 Q1009 - Answer1 참고
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine());
+		sc.close();
+	}
+}
---
+++ Q1001~Q1100/Q1022/Answer2.java
@@ -0,0 +1,20 @@
+package Q1022;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1022
+//@How BufferedReader
+//@Memory 11100, @Time 83
+
+public class Answer2 {
+
+	public static void main(String[] args) throws IOException {
+		// BufferedReader, StringBuilder에 대한 설명은 Q1009 - Answer2 참고
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());
+		br.close();
+	}
+}
