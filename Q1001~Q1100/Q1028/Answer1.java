8daa22b6e49e06b8360da90d6239ca893838f58d
plitche <kysu728@naver.com>
2021-01-14 01:58:27 +0900

    Add solution of Q1028
    Add solution of CodeUp's Question No.1028
    This one is really easy.
    just print what you have input.

---
+++ Q1001~Q1100/Q1028/Answer1.java
@@ -0,0 +1,18 @@
+package Q1028;
+
+import java.util.Scanner;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1028
+//@How Scanner, Math.pow()
+//@Memory 12432, @Time 115
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLong());	//입력받자마자 출력
+		sc.close();
+	}
+}
+
