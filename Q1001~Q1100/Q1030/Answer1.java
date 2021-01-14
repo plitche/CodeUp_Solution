0061abad20a2c188e114745fe43e89b85ee9d149
plitche <kysu728@naver.com>
2021-01-15 03:03:02 +0900

    Add solution of Q1030
    Add solution of CodeUp's Question No.1030
    This Question is also easy to solve.
    just careful about scope of number

---
+++ Q1001~Q1100/Q1030/Answer1.java
@@ -0,0 +1,18 @@
+package Q1030;
+
+import java.util.Scanner;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1030
+//@How Scanner
+//@Memory 12464, @Time 116
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLong());
+		sc.close();
+	}
+}
+
---
+++ Q1001~Q1100/Q1030/Answer2.java
@@ -0,0 +1,19 @@
+package Q1030;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1030
+//@How BufferedReader
+//@Memory 11100, @Time 82
+
+public class Answer2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());
+		br.close();
+	}
+}
