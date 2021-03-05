b4ba0efdbd0ee1366106330027087c1e1b83b907
plitche <kysu728@naver.com>
2021-03-05 10:36:03 +0900

    Add solution of Q1102
    Add solution of CodeUp's Question Q1102
    This question is about printing String ststement with change lines

---
+++ Q1101~Q1200/Q1102/Answer1.java
@@ -0,0 +1,15 @@
+package Q1102;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1102
+//@Memory 10992, @Time 56
+
+public class Answer1 {
+	// 이 문제는 Hello, World!를 줄바꾸어 출력하시오.
+	
+	public static void main(String[] args) {
+		System.out.println("Hello,");
+		System.out.print("World!");
+	}
+
+}
