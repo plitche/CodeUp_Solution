497ad56c1451ef08440a0538b5f1eb22ac0c0617
plitche <kysu728@naver.com>
2021-03-05 10:35:17 +0900

    Add solution of Q1101
    Add solution of CodeUp's Question Q1101
    This qusetion is about printing the String statement

---
+++ Q1101~Q1200/Q1101/Answer1.java
@@ -0,0 +1,14 @@
+package Q1101;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1101
+//@Memory 13856, @Time 93
+
+public class Answer1 {
+	// 이 문제는 Hello, World!를 출력하면 되는 문제입니다.
+	
+	public static void main(String[] args) {
+		System.out.printf("Hello, World!");
+	}
+
+}
