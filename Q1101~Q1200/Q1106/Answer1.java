a4cd56a13f1c645053f3d12517fcaeb77d891744
plitche <kysu728@naver.com>
2021-03-08 19:45:07 +0900

    Add solution of Q1106
    Add solution of CodeUp's Question Q1106
    This question is about arrangement of Int type.
    With Integer have to print Min and Max number.

---
+++ Q1101~Q1200/Q1106/Answer1.java
@@ -0,0 +1,14 @@
+package Q1106;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1106
+//@Memory 11008, @Time 64
+
+public class Answer1 {
+	// ﻿int형의 가장 작은수a와 int형의 가장 큰수b를 차례대로 공백으로 분리하여 출력한다.
+	
+	public static void main(String[] args) {
+		System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
+	}
+
+}
