60d2dab9a840b739c3bc0dabed5b0a3e247fe5d0
plitche <kysu728@naver.com>
2021-03-08 19:46:22 +0900

    Add solution of Q1107
    Add solution of CodeUp's Question Q1107
    There're various way to collect.
    The easiest way is to use for statement.

---
+++ Q1101~Q1200/Q1107/Answer1.java
@@ -0,0 +1,16 @@
+package Q1107;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1107
+//@Memory 11024, @Time 61
+
+public class Answer1 {
+	// ﻿int형의 가장 작은수a와 int형의 가장 큰수b를 차례대로 공백으로 분리하여 출력한다.
+	
+	public static void main(String[] args) {
+		for (int i=0; i<50; i++) {
+			System.out.print("print");
+		}
+	}
+
+}
