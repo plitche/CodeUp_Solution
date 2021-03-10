062f6e87f22637f0ba3e3ac97fcbc2306b616ac6
plitche <kysu728@naver.com>
2021-03-10 21:27:26 +0900

    Add solution of Q1112
    Add solution of CodeUp's Question Q1112
    This questiosn is about printing the numbers what you have input with one space.

---
+++ Q1101~Q1200/Q1112/Answer2.java
@@ -0,0 +1,27 @@
+package Q1112;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1112
+//@Memory 11240, @Time 61
+
+public class Answer2 {
+	// 입력 받은 두 정수를 출력한다.
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.append(br.readLine());
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
