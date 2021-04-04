c6991e49856a888cc57d38a63d114b71ae5a9ba3
plitche <kysu728@naver.com>
2021-04-04 21:41:09 +0900

    Add solution of Q1230
    Add solution of CodeUp's Question Q1230
    This question can be solved if & if else statement.

---
+++ Q1201~Q1300/Q1230/Answer1.java
@@ -0,0 +1,41 @@
+package Q1230;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1230
+//@Memory 11164, @Time 151
+
+public class Answer1 {
+	//	어떤 차의 높이가 170cm 이다.
+	//  터널 a,b,c의 높이가 공백으로 분리되어 차례대로 3개 입력된다. (정수)
+	//	높이가 170인 차가 잘 통과할 수 있으면 PASS를 출력, 170보다 같거나 작으면 CRASH 처음 충돌한 터널 높이를 출력하시오.
+	 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        // a, b, c 순서대로 통화 => 순서대로 조건문 작성
+        if (a<=170) {
+        	sb.append("CRASH").append(" "+a);
+        } else if (b<=170) {
+        	sb.append("CRASH").append(" "+b);
+        } else if (c<=170) {
+        	sb.append("CRASH").append(" "+c);
+        } else {
+        	sb.append("PASS");	//모두 통과하면 pass
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+
+}
