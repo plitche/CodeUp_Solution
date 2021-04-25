e2907442abc108fff278c803343eb15a59a8a647
plitche <kysu728@naver.com>
2021-04-25 15:05:18 +0900

    Add solution of Q1414
    Add solution of CodeUp's Question Q1414

---
+++ Q1401~Q1500/Q1414/Answer1.java
@@ -0,0 +1,41 @@
+package Q1414;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1414
+//@Memory 11108, @Time 108
+
+public class Answer1 {
+	//	문자열에서 “C”라는 문자와 “CC”라는 문자가 몇 개 있는지 조사하고자 한다.
+	//	① 첫 번째 줄에는 문자열에서 찾은 “C”의 개수를 출력한다.
+	//	② 두 번째 줄에는 문자열에서 찾은 “CC”의 개수를 출력한다.
+	
+    public static void main(String[] args) throws IOException {
+    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         String line = br.readLine();
+
+         int cCount = 0;
+         int ccCount = 0;
+         
+         for (int i=0; i<line.length(); i++) {
+        	 // c의 개수 구하기
+        	 if (line.charAt(i)=='c' || line.charAt(i)=='C') {
+        		 cCount++;
+        	 }
+        	 // cc의 개수 구하기
+        	 if (i<=line.length()-2) {
+        		 String a = line.substring(i, i+2);
+        		 if (a.equals("CC") || a.equals("Cc") || a.equals("cC") || a.equals("cc")) {
+        			 ccCount++;
+        		 }
+        	 }
+         }
+         
+         System.out.println(cCount);
+         System.out.println(ccCount);
+    }
+	
+}
