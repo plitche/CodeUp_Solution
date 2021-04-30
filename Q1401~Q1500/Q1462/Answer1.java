1d246ad5e0fa19b2d38547dc61963ad792d62a3b
plitche <kysu728@naver.com>
2021-04-30 23:24:12 +0900

    Add solution of Q1462
    Add solution of CodeUp's Question Q1462

---
+++ Q1401~Q1500/Q1462/Answer1.java
@@ -0,0 +1,32 @@
+package Q1462;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1462
+//@Memory 12508, @Time 82
+
+public class Answer1 {
+	//	두 배열을 합쳐서 오름차순 정렬된 배열을 출력하시오.
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        // 입력받을 숫자의 개수 만큼 for문 반복
+        for (int i=1; i<=n; i++) {
+        	for (int j=i; j<=n*n; j+=n) {
+        		sb.append(j).append(" ");
+        	}
+        	sb.append("\n");
+        }
+        
+        System.out.println(sb);
+        br.close();
+    }
+    	 
+}
