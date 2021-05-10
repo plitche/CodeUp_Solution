3de0984ca4842d4fed0b6e6b71054dd402385255
plitche <kysu728@naver.com>
2021-05-11 00:42:09 +0900

    edit package name

---
+++ Q1501~Q1599/Q1507/Answer1.java
@@ -0,0 +1,56 @@
+package Q1507;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1507
+//@Memory 11208, @Time 96
+
+public class Answer1 {
+	//	다음과 같은 n*m 배열 구조를 출력해보자.
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int[][] arr = new int[4][4];	// 4개의 직사각형 꼭지점을 저장할 배열
+        
+        StringTokenizer st;
+
+        // 4개의 직사각형임으로 4번 반복
+        for (int i=0; i<4; i++) {
+        	st = new StringTokenizer(br.readLine(), " ");
+        	for (int j=0; j<4; j++) {
+        		arr[i][j] = Integer.parseInt(st.nextToken());
+        	}
+        }
+        
+        // 직사각형의 넓이를 저장할 배열
+        int[][] wide = new int[99][99];
+        
+        for (int i=0; i<4; i++) {
+        	// 직사각형 가로 범위만큼 for문 반복
+        	for (int j=arr[i][0]; j<arr[i][2]; j++) {
+        		// 직사각형 세로 범위만큼 for문 반복
+        		for (int k=arr[i][1]; k<arr[i][3]; k++) {
+        			wide[j][k] = 1; // =색칠하기
+        		}
+        	}
+        }
+
+        int result = 0;
+        for (int[] i : wide) {
+        	for (int j : i) {
+        		if (j==1) {
+        			result++;
+        		}
+        	}
+        }
+        
+        System.out.println(result);
+        br.close();
+    }
+    	 
+}
