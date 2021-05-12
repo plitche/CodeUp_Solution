fa4aa308b38aa948215aeb09c1eaf5f45d78983a
plitche <kysu728@naver.com>
2021-05-13 00:12:50 +0900

    Add solution of Q1531
    Add solution of CodeUp's Question Q1531

---
+++ Q1501~Q1599/Q1531/Answer1.java
@@ -0,0 +1,12 @@
+package Q1531;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1531
+//@Memory 10868, @Time 91
+public class Answer1 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+    
+}
