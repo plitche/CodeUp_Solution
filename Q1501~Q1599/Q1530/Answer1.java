7f55c225908e43fdbb0c665d7cefbba8c663c4a2
plitche <kysu728@naver.com>
2021-05-13 00:12:44 +0900

    Add solution of Q1530
    Add solution of CodeUp's Question Q1530

---
+++ Q1501~Q1599/Q1530/Answer1.java
@@ -0,0 +1,12 @@
+package Q1530;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1530
+//@Memory 11008, @Time 87
+public class Answer1 {
+
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+    
+}
