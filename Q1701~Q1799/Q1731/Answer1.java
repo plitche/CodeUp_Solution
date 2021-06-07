8e021a89d03dcbe53e903fd7748050d8808fd78c
plitche <kysu728@naver.com>
2021-06-07 21:35:01 +0900

    Add solution of Q1731
    Add solution of CodeUp's Question Q1731

---
+++ Q1701~Q1799/Q1731/Answer1.java
@@ -0,0 +1,13 @@
+package Q1731;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1731
+//@Memory 11008, @Time 58
+
+public class Answer1 {
+	
+	public static void main(String[] args) {
+        System.out.println("special characters\r\n" + "[\\n,\\\",\\\\] is very important.");
+    }
+    	 
+}
