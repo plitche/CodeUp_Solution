655683c41c19e68e68ab1c9325f3b04225577190
plitche <kysu728@naver.com>
2021-06-11 23:19:01 +0900

    Add solution of Q1760
    Add solution of CodeUp's Question Q1760

---
+++ Q1701~Q1799/Q1760/Answer1.java
@@ -0,0 +1,41 @@
+package Q1760;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1760
+//@Memory 11160, @Time 92
+
+public class Answer1 {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String Line = br.readLine();
+        String phoneNum = br.readLine();
+
+        char[] numbers = new char[10];
+        for (int i=0; i<numbers.length; i++) {
+        	numbers[i] = Line.charAt(i);
+        }
+        
+        for (int i=0; i<phoneNum.length(); i++ ) {
+        	if (phoneNum.charAt(i) == ' ') sb.append(' ');
+        	if (phoneNum.charAt(i) == numbers[0]) sb.append('0');
+        	if (phoneNum.charAt(i) == numbers[1]) sb.append('1');
+        	if (phoneNum.charAt(i) == numbers[2]) sb.append('2');
+        	if (phoneNum.charAt(i) == numbers[3]) sb.append('3');
+        	if (phoneNum.charAt(i) == numbers[4]) sb.append('4');
+        	if (phoneNum.charAt(i) == numbers[5]) sb.append('5');
+        	if (phoneNum.charAt(i) == numbers[6]) sb.append('6');
+        	if (phoneNum.charAt(i) == numbers[7]) sb.append('7');
+        	if (phoneNum.charAt(i) == numbers[8]) sb.append('8');
+        	if (phoneNum.charAt(i) == numbers[9]) sb.append('9');
+        }
+        
+        System.out.println(sb);
+	}
+    	 
+}
