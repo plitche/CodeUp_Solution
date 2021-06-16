0b9d5b5c5cbc4178a382537126032b610fd2eca0
plitche <kysu728@naver.com>
2021-06-16 22:56:26 +0900

    Add solution of Q1859
    Add solution of CodeUp's Question Q1859

---
+++ Q1801~Q1899/Q1859/Answer1.java
@@ -0,0 +1,38 @@
+package Q1859;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1859
+//@Memory 11868, @Time 73
+public class Answer1 {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int line = Integer.parseInt(br.readLine());
+        
+        enter(line);
+	}
+	
+	public static void enter(int line) {
+		if(line<=0) {
+			return;
+		}
+		
+		enter(line-1);
+		loop(line);
+		System.out.println();
+	}
+	
+	public static void loop(int line) {
+		if (line<=0) {
+			return;
+		}
+		
+		loop(line-1);
+		System.out.print("*");
+	}
+    	 
+}
