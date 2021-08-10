a256a3e9fd89e401aefa86225e748f5862da3279
plitche <kysu728@naver.com>
2021-08-10 20:37:24 +0900

    Add solution of Q1210
    Add solution of CodeUp's Question Q1210

---
+++ Q1201~Q1300/Q1210/Answer2.java
@@ -0,0 +1,39 @@
+package Q1210;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//@author Yongsoo Kwon
+//@URL https://www.codeup.kr/problem.php?id=1210
+//@Memory 11148, @Time 64
+
+public class Answer2 {
+	//	메뉴의 번호가 차례대로 두개 주어진다. (정수)
+	//	그 번호의 메뉴 칼로리를 계산하여 500보다 크면 "angry", 500이하면 "no angry"를 출력하시오.
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		// 총 칼로리를 0으로 초기화 선언
+		int total = 0;
+				
+		// 2개의 메뉴 숫자를 입력 받자마자 계산한다.
+		for (int i=0; i<2; i++) {
+			// 메뉴의 칼로리에 맞게 계산 
+			switch (st.nextToken()) {
+			case "1" : total+=400; break;
+			case "2" : total+=340; break;
+			case "3" : total+=170; break;
+			case "4" : total+=100; break;
+			default: total+=70; break;
+			}
+		}
+		// 삼항 연산자를 사용해 저장
+		System.out.print(total>500? "angry":"no angry");
+		br.close();
+	}
+
+}
