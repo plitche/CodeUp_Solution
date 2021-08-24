ebfca45e6e4be51179a5e90539ff96e8c6811316
plitche <kysu728@naver.com>
2021-08-24 21:53:02 +0900

    Add solution of Q1255
    Add solution of CodeUp's Question Q21255

--- Q1201~Q1300/Q1255/Answer2.java
+++ Q1201~Q1300/Q1255/Answer2.java
@@ -7,9 +7,9 @@ import java.util.StringTokenizer;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1255
-//@Memory 16160, @Time 257
+//@Memory 16172, @Time 235
 
-public class Answer1 {
+public class Answer2 {
 	//	소수 둘째 자리의 두 실수 a와 b가 입력으로 주어진다. a와 b사이의 수를 0.01간격으로 오름차순으로 출력하시오.
 	 
 	public static void main(String[] args) throws IOException {
@@ -20,12 +20,18 @@ public class Answer1 {
 		double a = Double.parseDouble(st.nextToken());
 		double b = Double.parseDouble(st.nextToken());
 		
-		// 문제에서 b가 a보다 같거나 크다고 정해져 있기 때문에
-		for (double i=a; i<=b; i+=0.01) {
-			sb.append(String.format("%.2f", i)+" ");
+		// 입력 받은 두 숫자 중 뭐가 큰지 모르기 때문에
+		if (a>b) {
+			for (double i=a; i<=b; i+=0.01) {	// 0.01간격
+				sb.append(String.format("%.2f", i)+" ");
+			}
+		} else {
+			for (double i=a; i<=b; i+=0.01) {
+				sb.append(String.format("%.2f", i)+" ");
+			}
 		}
 		
-		System.out.print(sb);
+		System.out.println(sb);
 	}
 	
 }
