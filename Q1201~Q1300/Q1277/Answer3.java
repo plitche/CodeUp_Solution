c431c50f28d192d6019f4f8e36845b39de755317
plitche <kysu728@naver.com>
2021-09-02 21:42:42 +0900

    Add solution of Q1277
    Add solution of CodeUp's Question Q1277

--- Q1201~Q1300/Q1277/Answer3.java
+++ Q1201~Q1300/Q1277/Answer3.java
@@ -3,37 +3,40 @@ package Q1277;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1277
-//@Memory 11216, @Time 64
 
-public class Answer1 {
+public class Answer3 {
 	//	첫 줄에 데이터의 개수 N(N은 홀수)이 입력되고, 그 다음 줄에 N개의 데이터가 입력된다.
 	//	여기서 첫번째 데이터, 중간 데이터, 마지막 데이터를 출력하시오.
 	 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		// 숫자가 몇개인지 입력받는다.
 		int n = Integer.parseInt(br.readLine());
+		// 공백을 기준으로 나눈다.
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int count = 0;
-		Map<Object, Object> map = new HashMap<Object, Object>();
-		// 첫줄에 입력받은 숫자만큼 for문 반복
-		for (int i=1; i<=n; i++) {
-			// 몇개인지 새어준다.
-			count++;
-			// 선언해 놓은 map에 입력 받을 때 마다 넣어준다.
-			map.put(i, st.nextToken());
+		
+		// 가장 첫번째 숫자는 무조건 저장한다.
+		sb.append(st.nextToken() + " ");
+		
+		// n이 1인 경우에는 숫자 하나만 출력해야 한다.
+		if (n!=1) {
+			// n-2번만큼 for문을 반복한다.
+			for (int i=0; i<n-2; i++) {
+				String number = st.nextToken();
+				if (i==(n-2)/2) {
+					sb.append(number + " "); //중간 숫자일때 sb에 저장한다.
+				}
+			}
+			// 마지막  숫자는 무조건 저장한다.
+			sb.append(st.nextToken());
 		}
 		
-		StringBuilder sb = new StringBuilder();
-		// map에서 꺼내어 append해준다. (홀수임으로 +1해주어야 한다.)
-		sb.append(map.get(1)+" ").append(map.get(count/2+1)+" ").append(map.get(count));
-		System.out.println(sb);
+		System.out.print(sb);
 		br.close();
 	}
 	
