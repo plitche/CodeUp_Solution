a1795f892d8228481ee1a8cb54913d4922524855
plitche <kysu728@naver.com>
2021-09-12 16:33:23 +0900

    edit runtime

--- Q1201~Q1300/Q1297/Answer1.java
+++ Q1201~Q1300/Q1297/Answer1.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1297
-//@Memory 11180, @Time 100
+//@Memory 11176, @Time 69
 
 public class Answer1 {
 	//	그림과 같이 길이가 n인 철판을 구부려서 단면이 직사각형인 ㄷ자 모양을 만들려고 한다.
