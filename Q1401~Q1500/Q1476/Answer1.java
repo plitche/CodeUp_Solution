eb0937a53dc63fe27aae86fef484c141d31bbea4
plitche <kysu728@naver.com>
2021-10-31 17:01:22 +0900

    edit runtime

--- Q1401~Q1500/Q1476/Answer1.java
+++ Q1401~Q1500/Q1476/Answer1.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1476
-//@Memory 12760, @Time 119
+//@Memory 13128, @Time 92
 
 public class Answer1 {
 	//	다음과 같은 n*m 배열 구조를 출력해보자.
