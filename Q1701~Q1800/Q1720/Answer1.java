479fba906ffab8f17e46d4a3c6c0b01466854531
plitche <kysu728@naver.com>
2021-12-12 14:59:39 +0900

    add description

--- Q1701~Q1800/Q1720/Answer1.java
+++ Q1701~Q1800/Q1720/Answer1.java
@@ -20,27 +20,28 @@ public class Answer1 {
         
         int[] arr;
         int many = 0;
+        // 테스트케이스 개수만큼 반복
         for(int i=0; i<count; i++) {
         	st  = new StringTokenizer(br.readLine(), " ");
         	arr = new int[3];
         	
         	int result;
-        	for (int j=0; j<3; j++) {
+        	for (int j=0; j<3; j++) { // 각 테스트케이스마다 토큰에서 꺼내어 배열에 저장
         		arr[j] = Integer.parseInt(st.nextToken());
         	}
-        	result = Integer.parseInt(st.nextToken());
-        	Arrays.sort(arr);
+        	result = Integer.parseInt(st.nextToken()); // 결과여야 하는 답이 마지막
+        	Arrays.sort(arr); // 정렬
         	
-        	if (arr[0] == result) {
+        	if (arr[0] == result) { 
         		
-        	} else {
+        	} else { // 답이 일치하지 않을때 처리
         		many++;
         		sb.append(i+1).append(" ").append(arr[0]);
         	}
         	
         }
         
-        if (many == 0) {
+        if (many == 0) { // 틀린 케이스가 0개면 -1출력
         	sb.append("-1");
         }
         
--- Q1701~Q1800/Q1721/Answer1.java
+++ Q1701~Q1800/Q1721/Answer1.java
@@ -16,15 +16,15 @@ public class Answer1 {
         StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
         StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         
-        int x1 = Integer.parseInt(st1.nextToken());
-        int y1 = Integer.parseInt(st1.nextToken());
-        int x2 = Integer.parseInt(st2.nextToken());
-        int y2 = Integer.parseInt(st2.nextToken());
+        int x1 = Integer.parseInt(st1.nextToken()); // 왼쪽 꼭지점 가로좌표
+        int y1 = Integer.parseInt(st1.nextToken()); // 왼쪽 꼭지점 세로좌표
+        int x2 = Integer.parseInt(st2.nextToken()); // 오른쪽 꼭지점 가로좌표
+        int y2 = Integer.parseInt(st2.nextToken()); // 오른쪽 꼭지점 가로좌표
         
         int width = x2-x1;
         int height = y2-y1;
         
-        double length = Math.sqrt(width*width+height*height);
+        double length = Math.sqrt(width*width+height*height); // 거리 계산
         
         System.out.println(String.format("%.2f", length));
     }
