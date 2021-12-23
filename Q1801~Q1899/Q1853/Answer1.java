71b71d3a575cced89cd817d607d49985bf9df2f5
plitche <kysu728@naver.com>
2021-12-23 16:53:40 +0900

    edit rumtime and add description

--- Q1801~Q1899/Q1853/Answer1.java
+++ Q1801~Q1899/Q1853/Answer1.java
@@ -6,21 +6,21 @@ import java.io.InputStreamReader;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1853
-//@Memory 11172, @Time 95
+//@Memory 11196, @Time 69
 public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        
+        //  시작,  종료,  합계
         loop(1, count, 0);
 	}
 	
 	public static void loop(int start, int end, int total) {
 		if (start>end) {
-			System.out.println(total);
+			System.out.println(total); // 시작이 종료보다 더 작으면 출력
 		} else {
-			loop(start+1, end, total+start);
+			loop(start+1, end, total+start); // 시작을 +1 해주어 재귀함수 호출
 		}
 		
 	}
--- Q1801~Q1899/Q1854/Answer1.java
+++ Q1801~Q1899/Q1854/Answer1.java
@@ -6,13 +6,14 @@ import java.io.InputStreamReader;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1854
-//@Memory 11136, @Time 109
+//@Memory 11156, @Time 68
 public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String number = br.readLine();
+        String number = br.readLine(); // 어짜피 charAt을 이용할것이기 때문에 문자열로 받는다.
         
+        // 인덱스, 입력받은 숫자, 총 합
         loop(0, number, 0);
 	}
 	
@@ -21,6 +22,7 @@ public class Answer1 {
 		if (index==number.length()) {
 			System.out.println(total);
 		} else {
+			// 각자리 숫자를 charAt을 이용하여 더한다.
 			loop(index+1, number, total+number.charAt(index)-48);
 		}
 		
