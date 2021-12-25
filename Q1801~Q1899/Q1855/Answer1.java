7916e104421a8ea5b26d51264b6af286c2573f72
plitche <kysu728@naver.com>
2021-12-25 22:55:23 +0900

    edit rumtime and add description

--- Q1801~Q1899/Q1855/Answer1.java
+++ Q1801~Q1899/Q1855/Answer1.java
@@ -6,18 +6,18 @@ import java.io.InputStreamReader;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1855
-//@Memory 11156, @Time 88
+//@Memory 11172, @Time 70
 public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
-        
+        // 해당 자리, 몇 번째, 이전 값, 이후 값
         loop(0, count, 1, 2);
 	}
 	
 	public static void loop(int index, int count, int bef, int aft) {
-		if (count==1 || count==2) {
+		if (count==1 || count==2) { // 1,2 中 한가지 숫자가 들어왔을 때에는 그냥 1 출력(피보나치 수열)
 			System.out.println(1);
 			return;
 		}
--- Q1801~Q1899/Q1856/Answer1.java
+++ Q1801~Q1899/Q1856/Answer1.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=1856
-//@Memory 11328, @Time 95
+//@Memory 11304, @Time 77
 public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
@@ -18,14 +18,14 @@ public class Answer1 {
 	
 	public static int loop(int count) {
 
-		if (count==1) {
+		if (count==1) { // 남은 갯수가 1개일때는 당연히 1번
 			return 1;
-		} else if (count==2) {
-			return 1 + loop(1);
-		} else  if (count==3) {
+		} else if (count==2) { // 남은 갯수가 2개일 떄는 1번 + 1개일떄 경우의 수
+			return 1 + loop(1); 
+		} else  if (count==3) { // 남은 갯수가 3개 일때는 1번 + 1개일때 경우의 수 + 2개일때 경우의수
 			return 1 + loop(1) + loop(2);
 		} else {
-			return loop(count-3) + loop(count-2) + loop(count-1);
+			return loop(count-3) + loop(count-2) + loop(count-1); // 위 루프만큼 돌리기
 		}
 		
 	}
