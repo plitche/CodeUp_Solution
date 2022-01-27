44e4a0a0cc19bc2f9e1c894dadf23245b13dafe9
plitche <kysu728@naver.com>
2022-01-27 22:21:59 +0900

    edit rumtime and add description

--- Q2001~Q2100/Q2009/Answer1.java
+++ Q2001~Q2100/Q2009/Answer1.java
@@ -7,21 +7,21 @@ import java.util.StringTokenizer;
 
 //@author Yongsoo Kwon
 //@URL https://www.codeup.kr/problem.php?id=2009
-//@Memory 11160, @Time 69
+//@Memory 11184, @Time 66
 public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int coupone = Integer.parseInt(st.nextToken());
-        int count = Integer.parseInt(st.nextToken());
+        int coupone = Integer.parseInt(st.nextToken()); // 쿠폰 개수 입력 받기
+        int count = Integer.parseInt(st.nextToken()); // 필요 쿠폰 개수 입력 받기
         
         int coffee = 0;
-        while (coupone>=count) {
+        while (coupone>=count) { // 잔여 쿠폰 개수가 필요 쿠폰 개수보다 많을때
         	int share = 0;
-        	coffee += coupone/count;
-        	share = coupone/count;
-        	coupone = coupone%count+share;
+        	coffee += coupone/count; // 커피 개수를 잔여쿠폰/필요쿠폰 몫 만큼 더하기   
+        	share = coupone/count; // 몫을 share변수에 저장
+        	coupone = coupone%count+share; // 남은 쿠폰 개수를 잔여쿠폰%필요쿠폰 나머지에 그 몫을 더한다. 
         }
         
         System.out.println(coffee);
