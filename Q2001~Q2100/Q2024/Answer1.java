57f8436e851456d43870beb0f35670b9c05e17bb
plitche <kysu728@naver.com>
2022-02-03 01:29:13 +0900

    add description

--- Q2001~Q2100/Q2024/Answer1.java
+++ Q2001~Q2100/Q2024/Answer1.java
@@ -16,10 +16,12 @@ public class Answer1 {
         
         boolean isExist = false;
         
-        if (number.equals("0")) {
+        if (number.equals("0")) { // 먼저 입력받은 숫자가 0인지 먼저 체크
         	sb.append("영");
         } else {
+        	 // 가장 뒤의 자리부터 for문 반복 : 가장 뒷 자리부터 하는 이유는 StringBuilder에 저장하는 순서 때문
         	for (int i=number.length()-1; i>=0; i--) {
+        		 //각 자리 숫자를 charAt을 통해 얻어, switch문을 이용하여 해당 숫자를 문자로 저장
             	switch(number.charAt(number.length()-i-1)-48) {
             	case 1 : sb.append("일"); break;
             	case 2 : sb.append("이"); break;
@@ -32,9 +34,10 @@ public class Answer1 {
             	case 9 : sb.append("구"); break;
             	}
             	
+            	// 해당 자리 숫자가 0이 아닐 때 해당 자리수의 길이를 통해 단위 저장
             	if (number.charAt(number.length()-i-1) != '0') {
             		switch(i) {
-                	case 9 : sb.append("십"); isExist=true; break;
+                	case 9 : sb.append("십"); isExist=true; break; // 억&만 단위를 위한 boolean 변수 true
                 	case 8 : sb.append(""); isExist=true; break;
                 	case 7 : sb.append("천"); isExist=true; break;
                 	case 6 : sb.append("백"); isExist=true; break;
@@ -47,8 +50,9 @@ public class Answer1 {
                 	}
             	}
             	
+            	// 억&만 단위를 위한 boolean 변수 true일 때
             	if (isExist) {
-            		if (i==8) {
+            		if (i==8) { // 해당 자리수를 통해 억&만 저장
             			sb.append("억");
             			isExist=false;
             		} else if(i==4) {
