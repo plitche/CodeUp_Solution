f89cf70a0d95d0458edecf6a58767a03c0e03ffd
plitche <kysu728@naver.com>
2022-02-27 15:11:16 +0900

    add description

--- Q2001~Q2100/Q2051/Answer1.java
+++ Q2001~Q2100/Q2051/Answer1.java
@@ -14,19 +14,20 @@ public class Answer1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         
-        int score = 0;
-        for (int i=0; i<4; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
-        	for (int j=0; j<4; j++) {
-        		int number = Integer.parseInt(st.nextToken());
-        		if (number>2) {
+        int score = 0; // 0으로 초기화
+        for (int i=0; i<4; i++) { // for문 4번 반복
+        	st = new StringTokenizer(br.readLine(), " "); // 반복할떄마다 한줄을 StringTokenizer로 쪼갠다.
+        	for (int j=0; j<4; j++) { // inner for문 4번 반복
+        		int number = Integer.parseInt(st.nextToken()); // st에서 하나씩 꺼내어
+        		if (number>2) { // 해당 숫자가 2보다 큰 경우에
         			for (int k=4; k<=number; k*=2) {
-        				score+=number;
+        				score+=number; // 총점에 해당 숫자를 더해준다.
             		}	
         		}
         	}
         }
         
-    	System.out.println(score-(4*Integer.parseInt(br.readLine())));	
+        // 마지막에 입력받는 줄 *4를 총점에서 뺀 결과를 출력.
+    	System.out.println(score-(4*Integer.parseInt(br.readLine()))); 
 	}
 }
