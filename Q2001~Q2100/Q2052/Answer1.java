63c811303a6a40218c553a5c794c0b5d65ff904b
plitche <kysu728@naver.com>
2022-03-05 00:57:49 +0900

    add description

--- Q2001~Q2100/Q2052/Answer1.java
+++ Q2001~Q2100/Q2052/Answer1.java
@@ -15,18 +15,18 @@ public class Answer1 {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-        for (int i=0; i<5; i++) {
-        	int total = 0;
-        	int number = Integer.parseInt(st.nextToken());
+        for (int i=0; i<5; i++) { // 5개의 정수를 입력 받기로 했음으로 5번 반복
+        	int total = 0; // 총 합 0으로 초기화
+        	int number = Integer.parseInt(st.nextToken()); // 숫자 입력 받기
         	
-        	for (int j=1; j<=Math.sqrt(number); j++) {
-        		if (number%j == 0) {
-        			total+=j;
-        			total+=number/j;
+        	for (int j=1; j<=Math.sqrt(number); j++) { // 제곱근 까지 반복
+        		if (number%j == 0) { // 나누어 떨어질 때
+        			total+=j;	// 해당 숫자를 총 합에 더한다.
+        			total+=number/j; // 그리고 나눈 몫도 총 합에 더한다.
         		}
         	}
         	
-        	if (total-number == number) {
+        	if (total-number == number) { // 완전수인지 판단하여 yes or no 를 출력
         		sb.append("yes\n");
         	} else {
         		sb.append("no\n");
--- Q2001~Q2100/Q2055/Answer1.java
+++ Q2001~Q2100/Q2055/Answer1.java
@@ -17,31 +17,31 @@ public class Answer1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int a = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken()); // 두개의 숫자 입력 받기
         int b = Integer.parseInt(st.nextToken());
         
-        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>(); // 공통 약수를 저장할 list 선언
 
-    	for (int i=1; i*i<=b; i++) {
-    		if (a%i == 0) {
-    			list.add(i);
+    	for (int i=1; i*i<=b; i++) { // 1부터 b의 제곱근까지 반복(문제 조건에서 a<=b임으로)
+    		if (a%i == 0) { // 해당 숫자가 나누어 떨어질 때 
+    			list.add(i); // 리스트에 해당 숫자와 몫을 add 한다.
     			list.add(a/i);
     		}
-    		if (b%i == 0) {
+    		if (b%i == 0) { // a와 동일하게 해당 숫자로 나누어 떨어 질 때 리스트에 해당 숫자와 몫을 저장 
     			list.add(i);
     			list.add(b/i);
     		}
     	}
     	
-    	Collections.sort(list);
+    	Collections.sort(list); // 리스트 정렬
     	
-    	int pre = list.get(0);
-    	sb.append(pre + " ");
-    	for (int i=1; i<list.size(); i++) {
-    		int after = list.get(i);
-    		if (pre==after) {
+    	int pre = list.get(0); // 첫번째 숫자 선언
+    	sb.append(pre + " "); // 첫번째 숫자는 일단 먼저 sb에 저장
+    	for (int i=1; i<list.size(); i++) { // 해당 리스트의 사이즈(길이)만큼 반복
+    		int after = list.get(i); // 그 다음 숫자 꺼내기
+    		if (pre==after) { // 꺼내어 처음 숫자와 동일하면 넘어감
     			continue;
-    		} else {
+    		} else { // 동일하지 않으면 sb에 넣어준 다음 처음 숫자를 다음 숫자로 바꿔줌
     			sb.append(after + " ");
     			pre=after;
     		}
