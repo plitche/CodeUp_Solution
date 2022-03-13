013dc60f4d50930e2088c06801303d8eddae32f9
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-13 22:17:00 +0900

    Update Answer1.java
    add description

--- Q2501~Q2600/Q2502/Answer1.java
+++ Q2501~Q2600/Q2502/Answer1.java
@@ -16,25 +16,21 @@ public class Answer1 {
         int count = Integer.parseInt(br.readLine());
         StringTokenizer st;
         
-        for (int i=0; i<count; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
-        	int n = Integer.parseInt(st.nextToken());
+        for (int i=0; i<count; i++) { // 입력 받은 count 만큼 반복
+        	st = new StringTokenizer(br.readLine(), " "); // StringTokenizer를 통해 공백 기준으로 자르기
+        	int n = Integer.parseInt(st.nextToken()); // 두 정수 변수에 저장
         	int k = Integer.parseInt(st.nextToken());
         	
-        	if (k<n) {
+        	if (k<n) { // 조건에 따라 StringBuilder에 값 저장
         		sb.append("2\n");
-        	} else {
-        		if (k*2%n==0) {
-            		sb.append(k*2/n).append("\n");
+        	} else if (k*2%n==0) { // k의 2배를 n으로 나누었을 때 나누어 떨어지는 경우
+			sb.append(k*2/n).append("\n");  // 나누었을 때 몫
             	} else {
-            		sb.append(k*2/n+1).append("\n");
-            	}	
+            		sb.append(k*2/n+1).append("\n"); // n+1로 나누었을 때 몫
         	}
         }
         
         System.out.print(sb);
-        
 	}
-    	 
 }
 
