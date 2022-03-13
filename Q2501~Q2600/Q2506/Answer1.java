4af0c43e876b0a0f82137de0c812432cb9862c67
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-13 22:34:53 +0900

    Update Answer1.java
    Add description

--- Q2501~Q2600/Q2506/Answer1.java
+++ Q2501~Q2600/Q2506/Answer1.java
@@ -11,23 +11,23 @@ public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Double number = Double.parseDouble(br.readLine());
+        Double number = Double.parseDouble(br.readLine()); // 숫자 입력받기 (double 타입으로)
         
-        number *= 100;
-        int a = (int) Math.round(number);
+        number *= 100; // 해당 숫자에서 * 100
+        int a = (int) Math.round(number); // 반올림
         int b = 10000;
         
-        int index=a;
-        while(index!=1) {
-        	if (a%index==0 && b%index==0) {
-        		a /= index;
+        int index=a; // index 변수를 a로 초기화 선언
+        while(index!=1) { // 위에 선언한 index가 1이 될때까지 반복 (모든 정수는 1로 나누어 떨어짐으로 -> 1로 나누어 떨어지지 않을 때 까지 반복)
+        	if (a%index==0 && b%index==0) { // a와 b가 index로 나누어 떨어지면
+        		a /= index; // index의 몫으로 변수 저장
         		b /= index;
         	}
         	
-        	index--;
+        	index--; // index를 증감 하기
         }
         
-        System.out.print(a + "/" + b);
+        System.out.print(a + "/" + b); // 최종적으로 while문이 종료되면 더이상 나누어 떨어지지 않음. -> 출력
         
 	}
     	 
