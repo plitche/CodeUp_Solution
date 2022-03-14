76ff81326c4eca9e803baeb63b73f8c0a2d03de4
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-14 21:07:42 +0900

    Update Answer1.java
    add description

--- Q2601~Q2700/Q2601/Answer1.java
+++ Q2601~Q2700/Q2601/Answer1.java
@@ -11,19 +11,19 @@ public class Answer1 {
 	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(br.readLine());
+        int count = Integer.parseInt(br.readLine()); // 
         
-        int[] arr = new int[count];
-        if (count == 1 || count == 2) {
+        int[] arr = new int[count]; // 입력받은 count 개수만큼 배열의 길이 초기화
+        if (count == 1 || count == 2) { // 입력받은 count 1,2 인 경우 1출력
         	System.out.print(1);
         } else {
-        	arr[0] = 1;
+        	arr[0] = 1; // 배열의 첫번째 자리와 두번째 자리 1로 초기화
         	arr[1] = 1;
-        	for (int i=2; i<count; i++) {
-        		arr[i] = arr[i-1]+arr[i-2];
+        	for (int i=2; i<count; i++) { // 배열의 첫번째와 두번째자리를 초기화 했음으로 2부터 반복 시작
+        		arr[i] = arr[i-1]+arr[i-2]; // 수열의 규칙에 따라 계산하여 배열의 index자리 값 저장
         	}
         	
-        	System.out.print(arr[count-1]);
+        	System.out.print(arr[count-1]); // 마지막 자리 출력
         }
         
 	}
