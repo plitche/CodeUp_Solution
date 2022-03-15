60867b9e3c41b0566af207e0a4608d0d1af35f9a
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-15 22:35:13 +0900

    Update Answer1.java

--- Q2001~Q2100/Q2027/Answer1.java
+++ Q2001~Q2100/Q2027/Answer1.java
@@ -13,30 +13,30 @@ public class Answer1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(br.readLine());
         
-        int[] numbers = new int[count];
+        int[] numbers = new int[count]; // 입력받은 정수 숫자만큼 배열 길이 선정
         
         int last = 0;
         int first;
         int second;
         int sum = 1;
         
-        for (int i=2; i<count; i++) {
-        	numbers[0] = 1;
-            numbers[1] = 1;
+        for (int i=2; i<count; i++) { // index 2부터 for 반복문 시작
+        	numbers[0] = 1; // 0번째 index 자리를 1로 초기화
+            numbers[1] = 1; // 1번째 index자리를 1로 초기화
         	if (i == 2) {
-        		last = 1;
+        		last = 1; // index 가 2인 경우 1로 저장
         	} else {
-        		last = sum;	
+        		last = sum;	// 이외의 경우 합계를 변수에 저장
         	}
         	
-        	first = numbers[last-1]; 
-        	second = numbers[i-last];
-        	sum = first+second;
+        	first = numbers[last-1]; // 첫번째를 마지막 index - 1 자리의 value로 저장
+        	second = numbers[i-last]; // 두번쨰 변수를 index의 0 last 숫자 index의 value로 저장
+        	sum = first+second; // 합계를 첫번째, 두번쨰 값의 합으로 저장
         	
-        	numbers[i] = sum;
+        	numbers[i] = sum; // i번쨰 index value를 sum으로 저장
         }
         
-        System.out.println(sum);
+        System.out.println(sum); // sum 출력
 	}
 }
  
