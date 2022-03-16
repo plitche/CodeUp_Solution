cfe7a714acace49610e1ccbc5003e150b016763e
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-16 11:56:10 +0900

    Update Answer1.java

--- Q2001~Q2100/Q2048/Answer1.java
+++ Q2001~Q2100/Q2048/Answer1.java
@@ -16,26 +16,26 @@ public class Answer1 {
         StringTokenizer st;
         
         double total = 0;
-        for (int i=0; i<4; i++) {
+        for (int i=0; i<4; i++) {  // 4x4 입력 받아야 함으로, 2중 for문 4번씩 반복
         	st = new StringTokenizer(br.readLine(), " ");
         	for (int j=0; j<4; j++) {
-        		total+=Integer.parseInt(st.nextToken());
+        		total+=Integer.parseInt(st.nextToken()); // 반복 하면서 합계를 구해 total 변수에 저장
         	}
         }
 
-        while(true) {
-        	total/=2.0;
+        while(true) { // 무한 반복 진행 
+        	total/=2.0; // 총 합계를 2로 나눈다. (total <= 1 이 될때까지 반복)
 
-        	if (total==1) {
+        	if (total==1) { // 합계가 1이 되면 yes 출력 -> 통일이 됨
         		sb.append("yes");
         		break;
-        	} else if (total<1) {
+        	} else if (total<1) { // total이 1보다 작아지면 no -> 통일이 안됨
         		sb.append("no");
         		break;
         	}
         	
         }
         
-    	System.out.println(sb);	
+    	System.out.println(sb);	// 
 	}
 }
