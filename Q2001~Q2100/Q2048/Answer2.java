d701fff1bba2f2394ef137331fec18b08d28c63a
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-17 10:56:09 +0900

    Update Answer2.java

--- Q2001~Q2100/Q2048/Answer2.java
+++ Q2001~Q2100/Q2048/Answer2.java
@@ -15,23 +15,23 @@ public class Answer2 {
         StringTokenizer st;
         
         double total = 0;
-        for (int i=0; i<4; i++) {
-        	st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<4; i++) { // 4x4임으로 2중 for문 length 4만큼 반복
+        	st = new StringTokenizer(br.readLine(), " "); // 반복문이 돌 때마다 st에서 토큰 가져오기
         	for (int j=0; j<4; j++) {
-        		total+=Integer.parseInt(st.nextToken());
+        		total+=Integer.parseInt(st.nextToken()); // 합계 구하기
         	}
         }
 
         int index = 0;
-        while (true) {
-        	if (total == Math.pow(2, index)) {
+        while (true) { // 합계가 <= 1 이 될때까지 반복
+        	if (total == Math.pow(2, index)) { // 합계가 2의 index 거듭제곱과 일치하면 yes 출력
         		System.out.println("yes");
         		break;
-        	} else if (total < Math.pow(2, index)) {
+        	} else if (total < Math.pow(2, index)) { // 합계가 2의 index 거듭제곱 보다 작으면 no 출력
         		System.out.println("no");
         		break;
         	}
-        	index++;
+        	index++; // index 1 증가
         	
         }
 	}
