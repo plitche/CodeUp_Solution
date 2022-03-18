3eef3e48e6f0852860ea847124d5501aa7349250
plitche <kysu728@naver.com>
2022-03-18 22:33:52 +0900

    Add descirption
    Add descirption of Question 2081

--- Q2001~Q2100/Q2081/Answer1.java
+++ Q2001~Q2100/Q2081/Answer1.java
@@ -12,12 +12,12 @@ public class Answer1 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int max = Integer.parseInt(br.readLine());
-        int index = 1;
-        for (int i=0; i<8; i++) {
-        	int number = Integer.parseInt(br.readLine());
-        	if (number > max) {
-        		max = number;
+        int max = Integer.parseInt(br.readLine()); // 첫번 째 입력된 숫자를 max로 초기화
+        int index = 1; // index 초기화
+        for (int i=0; i<8; i++) { // 8번 반복
+        	int number = Integer.parseInt(br.readLine()); // 반복할때 마다 정수 입력받기
+        	if (number > max) { // 입력받은 숫자가 max 보다 크면 
+        		max = number; // max를 입력받은 새로운 숫자로 바꿈
         		index = i+2;
         	}
         }
