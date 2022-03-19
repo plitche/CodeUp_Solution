01bff7ecfbce0b4056d156342d9f8f045a5a74fe
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-03-19 10:10:08 +0900

    Update Answer1.java

--- Q2001~Q2100/Q2082/Answer1.java
+++ Q2001~Q2100/Q2082/Answer1.java
@@ -14,22 +14,22 @@ public class Answer1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int count = Integer.parseInt(st.nextToken());
-        int number = Integer.parseInt(st.nextToken());
+        int count = Integer.parseInt(st.nextToken()); // 수의 개수 입력받기
+        int number = Integer.parseInt(st.nextToken()); // 위치를 찾을 정수 입력 받기
 
         StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         
-        boolean isFind = false;
-        for (int i=0; i<count; i++) {
+        boolean isFind = false; // 찾았는지 여부 변수로 false 초기화
+        for (int i=0; i<count; i++) { // 수의 개수만큼 for문 반복
         	int no = Integer.parseInt(st2.nextToken());
-        	if (no == number) {
-        		isFind = true; 
-        		sb.append(i+1);
+        	if (no == number) { // 입력 받은 숫자가 number와 일치한지 체크
+        		isFind = true;  // boolean 변수 true로 변환
+        		sb.append(i+1); // i번째 index +1 (수열의 n번째)
         		break;
         	} 
         }
         
-        System.out.println(isFind ? sb : "-1");	
+        System.out.println(isFind ? sb : "-1");	// isFind가 true이면 sb 출력 아니면 -1 
     	
 	}
 }
