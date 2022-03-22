3f062d8b38fb12fe994c337514335226e093fcc8
plitche <kysu728@naver.com>
2022-03-22 21:02:57 +0900

    add description

--- Q2001~Q2100/Q2044/Answer1.java
+++ Q2001~Q2100/Q2044/Answer1.java
@@ -12,46 +12,46 @@ public class Answer1 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String line = br.readLine();
+        String line = br.readLine(); // 한 줄 입력받기
         
-        char[] arr = new char[line.length()];
+        char[] arr = new char[line.length()]; // 입력받은 문자열의 길이만큼 char타입 배열 길이 선언
         int i = 0;
         int index = 0;
-        while(true) {
-        	if (line.charAt(index) == 'A') {
+        while(true) { // true로 무한 반복 진행 -> 추후 조건에 따라 break
+        	if (line.charAt(index) == 'A') { // A가 입력되면 i-1
         		i -= 1;
-        		if (index == arr.length-1) {
+        		if (index == arr.length-1) { // index의 길이가 length의 길이-1 와 같다면 i-1 후 break;
         			i -= 1;
         			break;
         		}
-        	} else if (line.charAt(index) == 'B') {
+        	} else if (line.charAt(index) == 'B') {  // B가 입력되면 i-2
         		i -= 2;
-        		if (index == arr.length-1) {
+        		if (index == arr.length-1) { // index의 길이가 length의 길이-1 와 같다면 i-2 후 break;
         			i -= 2;
         			break;
         		}
-        	} else if (line.charAt(index) == 'C') {
+        	} else if (line.charAt(index) == 'C') {  // C가 입력되면 i를 0으로 초기화
         		i = 0;
-        		if (index == arr.length-1) {
+        		if (index == arr.length-1) {  // index의 길이가 length의 길이-1 와 같다면 break;
         			break;
         		}
-        	} else {
-        		arr[i] = line.charAt(index);
+        	} else {  // A,B,C 가 아닌 경우 
+        		arr[i] = line.charAt(index); // array의 i자리에 해당 index 문자 저장
 
-        		if (index<arr.length-1) {
+        		if (index<arr.length-1) { // index가 array의 길이-1 보다 작다면 i+1
         			i++;	
         		}
         	}
         	
-        	if (index<arr.length-1) {
+        	if (index<arr.length-1) { // index가 array의 길이-1 보다 작다면 i+1
         		index++;	
         	} else {
-        		break;
+        		break; // 반대의 경우 break
         	}
         	
         }
         
-    	for (int j=0; j<=i; j++) {
+    	for (int j=0; j<=i; j++) { // arr의 길이만큼 반복하여 Stringbuilder에 저장 후 출력
         	sb.append(arr[j]);
         }	
         
