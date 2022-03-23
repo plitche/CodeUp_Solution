1e6cbd60a13867e888507ba1e70011c5c52d832b
plitche <kysu728@naver.com>
2022-03-23 21:56:08 +0900

    add description

--- Q2001~Q2100/Q2061/Answer1.java
+++ Q2001~Q2100/Q2061/Answer1.java
@@ -14,20 +14,22 @@ public class Answer1 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int floor = 0;
-        switch(br.readLine()) {
-        case "A" : floor = 10; break;
-        case "S" : floor = 2; break;
-        case "O" : floor = 1; break;
+        switch(br.readLine()) { // 입력받은 문자에 따라 switch 조건문 진행
+        case "A" : floor = 10; break; // A가 입력되었을떄 floor 10으로 저장
+        case "S" : floor = 2; break; // S가 입력되었을떄 floor 2으로 저장
+        case "O" : floor = 1; break; // O가 입력되었을떄 floor 1으로 저장
         }
         
-        int t = Integer.parseInt(br.readLine());
-        int x = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine()); // 평수 캐스팅하여 정수로 입력받기
+        int x = Integer.parseInt(br.readLine()); // 무게 캐스팅하여 정수로 입력받기
         
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 돈과 풍선 개당 금액을 공백을 기준으로 입력받기
         int money = Integer.parseInt(st.nextToken());
         int price = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(br.readLine());
+        int w = Integer.parseInt(br.readLine()); // 한 풍선당 들수 있는 무게
         
+        // 삼항연산자를 이용
+        // (평수 + (층수*무게/한 풍선당 무게)*)가격   이 money+1 보다 큰 경우 No 작거나 같은경우 Yes
     	System.out.println((t + floor*x/w)*price > money +1 ? "No" : "Yes");	
     	
 	}
