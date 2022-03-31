f3f014313133c5cf50a6292e13aff0dea6b97801
plitche <kysu728@naver.com>
2022-03-31 20:25:10 +0900

    add description

--- Q2401~Q2500/Q2414/Answer1.java
+++ Q2401~Q2500/Q2414/Answer1.java
@@ -15,19 +15,19 @@ public class Answer1 {
         int people = Integer.parseInt(br.readLine());
         StringTokenizer st;
 
-        String[] name = new String[people];
-        int[] age = new int[people];
+        String[] name = new String[people]; // 학생의 숫자의 길이만큼 배열의 길이 지정
+        int[] age = new int[people]; // 나이를 저장할 정수형 배열의 길이 지정
 
-        for (int i=0; i<people; i++) {
-        	st = new StringTokenizer(br.readLine(), ",");
-        	name[i] = st.nextToken();
-        	st.nextToken();
-        	age[i] = Integer.parseInt(st.nextToken());
+        for (int i=0; i<people; i++) { // 학생의 숫자만큼 반복문 진행
+        	st = new StringTokenizer(br.readLine(), ","); // ,(쉼표)를 기준으로 한줄 입력 받기
+        	name[i] = st.nextToken(); // 가장 첫번째 자리의 token을 꺼내어 배열에 저장
+        	st.nextToken(); // 다음 성별은 불필요하기에 그냥 호출
+        	age[i] = Integer.parseInt(st.nextToken()); // 그 다음 나이 입력 받기
         }
         
-        String target = br.readLine();
+        String target = br.readLine(); // 찾아야되는 학생의 이름 입력 받기
         for (int i=0; i<people; i++) {
-        	if (name[i].equals(target)) {
+        	if (name[i].equals(target)) { // 이름 동일 여부 확인 후 해당 index자리의 나이 출력
         		System.out.println(age[i]);
         	}
         	
