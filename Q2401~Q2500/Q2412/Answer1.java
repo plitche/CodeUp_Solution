e3a7b822e4453f24c6f9601341141cb50ac86d90
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-04-13 19:02:49 +0900

    Update Answer1.java

--- Q2401~Q2500/Q2412/Answer1.java
+++ Q2401~Q2500/Q2412/Answer1.java
@@ -12,19 +12,19 @@ public class Answer1 {
 
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int people = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        int people = Integer.parseInt(br.readLine()); // 사람 숫자 입력받기
+        StringTokenizer st; 
         
         Double total = 0.0;
-        for (int i=0; i<people; i++) {
-        	st = new StringTokenizer(br.readLine(), ",");
-        	st.nextToken();
-        	st.nextToken();
-        	double age = Double.parseDouble(st.nextToken());
+        for (int i=0; i<people; i++) { // 사람의 숫자만큼 반복
+        	st = new StringTokenizer(br.readLine(), ","); // 반복문이 돌때마다 한줄씩 입력받아 ,를 기준으로 나누어 토큰화
+        	st.nextToken(); // 불필요 토큰
+        	st.nextToken(); // 불필요 토큰
+        	double age = Double.parseDouble(st.nextToken()); // 다음 토큰을꺼내어 double타입으로 변수에 저장
         	total += age;
         }
         
-        System.out.println(String.format("%.2f", total/people));
+        System.out.println(String.format("%.2f", total/people)); // 평균나이를 반올림하여 계산
 	}
 }
  
