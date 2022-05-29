dfd99ee0938a68d4992d097d37ca7e5728c94560
Yongsoo Kwon <70326085+plitche@users.noreply.github.com>
2022-05-29 15:32:43 +0900

    Update Answer1.java

--- Q2701~Q2800/Q2762/Answer1.java
+++ Q2701~Q2800/Q2762/Answer1.java
@@ -13,7 +13,7 @@ public class Answer1 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine(), ","); //쉼표를 기준으로 나눔
+        StringTokenizer st = new StringTokenizer(br.readLine(), ","); //쉼표를 토큰을 기준으로 나눔
         
         while(st.hasMoreTokens()) { // 토큰이 남아있을때까지 반복
         	sb.append(st.nextToken().charAt(0)); // 각토큰의 0번째 인덱스 값 sb에 append
